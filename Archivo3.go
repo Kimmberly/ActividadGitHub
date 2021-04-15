d62ad9157a16a27b72a9fd37931e30534f75b81f
Kimmberly <kimberly-torresj@unilibre.edu.co>
2021-04-15 16:32:27 -0500

    Actualizaciones y nuevo archivo

--- Archivo3.go
+++ Archivo3.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Archivo 3");
+	fmt.Println("Archivo 3 actualizado");
 }
--- Archivo4.go
+++ Archivo4.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Archivo 2 actualizado");
+	fmt.Println("Archivo 4");
 }
